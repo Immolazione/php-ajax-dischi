f8c3d9c78de66c3bad9de0e79bce6fb874f48e98
Immolazione <federico.deered@gmail.com>
2022-03-01 18:23:19 +0100

    add logo, minor fixes, import vue2

--- milestone1/index.php
+++ milestone1/index.php
@@ -16,23 +16,33 @@ include 'data.php';
     <!-- AXIOS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.26.0/axios.min.js" integrity="sha512-bPh3uwgU5qEMipS/VOmRqynnMXGGSRv+72H/N260MQeXZIK4PG48401Bsby9Nq5P5fz7hy5UGNmC/W1Z51h2GQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- VUE -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.31/vue.cjs.js" integrity="sha512-2e2aXOh4/FgkCAUyurkjk0Uw4m1gPcExFwb1Ai4Ajjg97se/FEWfrLG1na4mq8cgOzouc8qLIqsh0EGksPGdqQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.14/vue.common.dev.min.js" integrity="sha512-TpgbLHXaTCAZ7ULhjopb1PveTz5Jx6KEQUtMfXhV0m0EArh+6O8ybZjjDN1Yug6oagN6iFm6EoMjuYSiFr0qXQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link rel="stylesheet" href="css/style.css">
     <title>Vue Dischi</title>
 </head>
 <body>
-    <header></header>
-    <main>
-        <section id="card-section">
-            <?php foreach($albums as $album): ?> <!-- CICLO FOR QUA -->
-                <div id="card">
-                    <img src="<?= $album['poster'] ?>" alt="album_cover">
-                    <h1><?= $album['title'] ?></h1>
-                    <h5><?= $album['author'] ?></h5>
-                    <h6><?= $album['year'] ?></h6>
-                </div>
-            <?php endforeach; ?> <!-- FINE CICLO FOR QUA -->
-        </section>
-    </main>
+    <div id="root">
+        <header>
+            <img class="header-logo" src="img/spotify.png" alt="spotify_logo">
+            <!-- <div>
+                <select v-model="selectedOption">
+                    <option value="">{{ placeholder || 'Choose an Option:' }}</option>
+                    <option v-for="(option, index) in options" :key="option.id || index">{{ option }}</option>
+                </select>
+            </div> -->
+        </header>
+        <main>
+            <section id="card-section">
+                <?php foreach($albums as $album): ?> <!-- CICLO FOR QUA -->
+                    <div id="card">
+                        <img src="<?= $album['poster'] ?>" alt="album_cover">
+                        <h1><?= $album['title'] ?></h1>
+                        <h5><?= $album['author'] ?></h5>
+                        <h6><?= $album['year'] ?></h6>
+                    </div>
+                <?php endforeach; ?> <!-- FINE CICLO FOR QUA -->
+            </section>
+        </main>
+    </div>
 </body>
 </html>
---
+++ milestone1/js/script.js
@@ -0,0 +1,8 @@
+console.log('Vue Ok', Vue);
+
+Vue.config.devtools = true;
+
+const app = new Vue ({
+    eh: '#root',
+    data: {},
+}) 
